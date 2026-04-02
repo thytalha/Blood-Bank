26564dda92ea53be1b26d6a0c3790f9a5a0e4656
thytalha <mtpasha007@gmail.com>
2026-04-02 18:31:24 +0500

    Added a login form

--- MyForm.h
+++ MyForm.h
@@ -9,24 +9,15 @@ namespace BloodBank {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MyForm
-	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
 		~MyForm()
 		{
 			if (components)
@@ -35,7 +26,7 @@ namespace BloodBank {
 			}
 		}
 
-		// --- UI COMPONENTS ---
+	private: System::Windows::Forms::Panel^ loginPanel;
 	private: System::Windows::Forms::Label^ lblTitle;
 	private: System::Windows::Forms::Label^ lblUsername;
 	private: System::Windows::Forms::TextBox^ txtUsername;
@@ -45,21 +36,13 @@ namespace BloodBank {
 	private: System::Windows::Forms::Button^ btnSignup;
 	private: System::Windows::Forms::Label^ lblStatus;
 
-	protected:
-
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->loginPanel = (gcnew System::Windows::Forms::Panel());
 			this->lblTitle = (gcnew System::Windows::Forms::Label());
 			this->lblUsername = (gcnew System::Windows::Forms::Label());
 			this->txtUsername = (gcnew System::Windows::Forms::TextBox());
@@ -68,135 +51,132 @@ namespace BloodBank {
 			this->btnLogin = (gcnew System::Windows::Forms::Button());
 			this->btnSignup = (gcnew System::Windows::Forms::Button());
 			this->lblStatus = (gcnew System::Windows::Forms::Label());
+			this->loginPanel->SuspendLayout();
 			this->SuspendLayout();
-
+			// 
+			// loginPanel
+			// 
+			this->loginPanel->BackColor = System::Drawing::Color::Transparent;
+			this->loginPanel->Controls->Add(this->lblStatus);
+			this->loginPanel->Controls->Add(this->btnSignup);
+			this->loginPanel->Controls->Add(this->btnLogin);
+			this->loginPanel->Controls->Add(this->txtPassword);
+			this->loginPanel->Controls->Add(this->lblPassword);
+			this->loginPanel->Controls->Add(this->txtUsername);
+			this->loginPanel->Controls->Add(this->lblUsername);
+			this->loginPanel->Controls->Add(this->lblTitle);
+			this->loginPanel->Location = System::Drawing::Point(150, 100); // Temporary position
+			this->loginPanel->Name = L"loginPanel";
+			this->loginPanel->Size = System::Drawing::Size(400, 350);
+			this->loginPanel->TabIndex = 0;
 			// 
 			// lblTitle
 			// 
 			this->lblTitle->AutoSize = true;
-			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Segoe UI", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lblTitle->Location = System::Drawing::Point(55, 25);
+			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20, System::Drawing::FontStyle::Bold));
+			this->lblTitle->ForeColor = System::Drawing::Color::White;
+			this->lblTitle->Location = System::Drawing::Point(45, 20);
 			this->lblTitle->Name = L"lblTitle";
-			this->lblTitle->Size = System::Drawing::Size(262, 38);
-			this->lblTitle->TabIndex = 0;
+			this->lblTitle->Size = System::Drawing::Size(315, 46);
 			this->lblTitle->Text = L"Admin Login Portal";
 			// 
 			// lblUsername
 			// 
 			this->lblUsername->AutoSize = true;
-			this->lblUsername->Location = System::Drawing::Point(40, 100);
-			this->lblUsername->Name = L"lblUsername";
-			this->lblUsername->Size = System::Drawing::Size(73, 16);
-			this->lblUsername->TabIndex = 1;
+			this->lblUsername->ForeColor = System::Drawing::Color::White;
+			this->lblUsername->Location = System::Drawing::Point(50, 110);
 			this->lblUsername->Text = L"Username:";
 			// 
 			// txtUsername
 			// 
-			this->txtUsername->Location = System::Drawing::Point(130, 97);
+			this->txtUsername->Location = System::Drawing::Point(140, 107);
 			this->txtUsername->Name = L"txtUsername";
-			this->txtUsername->Size = System::Drawing::Size(170, 22);
-			this->txtUsername->TabIndex = 2;
+			this->txtUsername->Size = System::Drawing::Size(200, 22);
 			// 
 			// lblPassword
 			// 
 			this->lblPassword->AutoSize = true;
-			this->lblPassword->Location = System::Drawing::Point(40, 150);
-			this->lblPassword->Name = L"lblPassword";
-			this->lblPassword->Size = System::Drawing::Size(70, 16);
-			this->lblPassword->TabIndex = 3;
+			this->lblPassword->ForeColor = System::Drawing::Color::White;
+			this->lblPassword->Location = System::Drawing::Point(50, 160);
 			this->lblPassword->Text = L"Password:";
 			// 
 			// txtPassword
 			// 
-			this->txtPassword->Location = System::Drawing::Point(130, 147);
+			this->txtPassword->Location = System::Drawing::Point(140, 157);
 			this->txtPassword->Name = L"txtPassword";
 			this->txtPassword->PasswordChar = '*';
-			this->txtPassword->Size = System::Drawing::Size(170, 22);
-			this->txtPassword->TabIndex = 4;
+			this->txtPassword->Size = System::Drawing::Size(200, 22);
 			// 
 			// btnLogin
 			// 
-			this->btnLogin->BackColor = System::Drawing::Color::LightBlue;
-			this->btnLogin->Location = System::Drawing::Point(60, 210);
+			this->btnLogin->BackColor = System::Drawing::Color::DodgerBlue;
+			this->btnLogin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnLogin->ForeColor = System::Drawing::Color::White;
+			this->btnLogin->Location = System::Drawing::Point(70, 220);
 			this->btnLogin->Name = L"btnLogin";
-			this->btnLogin->Size = System::Drawing::Size(100, 35);
-			this->btnLogin->TabIndex = 5;
+			this->btnLogin->Size = System::Drawing::Size(110, 40);
 			this->btnLogin->Text = L"Login";
 			this->btnLogin->UseVisualStyleBackColor = false;
 			this->btnLogin->Click += gcnew System::EventHandler(this, &MyForm::btnLogin_Click);
 			// 
 			// btnSignup
 			// 
-			this->btnSignup->BackColor = System::Drawing::Color::LightGray;
-			this->btnSignup->Location = System::Drawing::Point(180, 210);
+			this->btnSignup->BackColor = System::Drawing::Color::SlateGray;
+			this->btnSignup->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnSignup->ForeColor = System::Drawing::Color::White;
+			this->btnSignup->Location = System::Drawing::Point(210, 220);
 			this->btnSignup->Name = L"btnSignup";
-			this->btnSignup->Size = System::Drawing::Size(100, 35);
-			this->btnSignup->TabIndex = 6;
+			this->btnSignup->Size = System::Drawing::Size(110, 40);
 			this->btnSignup->Text = L"Sign Up";
 			this->btnSignup->UseVisualStyleBackColor = false;
-			this->btnSignup->Click += gcnew System::EventHandler(this, &MyForm::btnSignup_Click);
 			// 
 			// lblStatus
 			// 
 			this->lblStatus->AutoSize = true;
-			this->lblStatus->Location = System::Drawing::Point(60, 260);
+			this->lblStatus->Location = System::Drawing::Point(70, 280);
 			this->lblStatus->Name = L"lblStatus";
-			this->lblStatus->Size = System::Drawing::Size(0, 16);
-			this->lblStatus->TabIndex = 7;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(360, 320);
-			this->Controls->Add(this->lblStatus);
-			this->Controls->Add(this->btnSignup);
-			this->Controls->Add(this->btnLogin);
-			this->Controls->Add(this->txtPassword);
-			this->Controls->Add(this->lblPassword);
-			this->Controls->Add(this->txtUsername);
-			this->Controls->Add(this->lblUsername);
-			this->Controls->Add(this->lblTitle);
+			this->BackColor = System::Drawing::Color::FromArgb(2, 20, 50); // NAVY BLUE BG
+			this->ClientSize = System::Drawing::Size(800, 600);
+			this->Controls->Add(this->loginPanel);
 			this->Name = L"MyForm";
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->WindowState = System::Windows::Forms::FormWindowState::Maximized; // Full-screen mode
 			this->Text = L"Blood Bank Interface";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->Resize += gcnew System::EventHandler(this, &MyForm::MyForm_Resize); // Handles centering on resize
+			this->loginPanel->ResumeLayout(false);
+			this->loginPanel->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
-
 		}
 #pragma endregion
 
-		// --- EVENT HANDLERS ---
-
-	private: System::Void btnLogin_Click(System::Object^ sender, System::EventArgs^ e) {
-		// Retrieve text from textboxes
-		String^ username = txtUsername->Text;
-		String^ password = txtPassword->Text;
-
-		// Basic validation example (Replace this with database logic later)
-		if (username == "admin" && password == "admin123") {
-			lblStatus->ForeColor = System::Drawing::Color::Green;
-			lblStatus->Text = "Login Successful! Welcome Admin.";
-
-			MessageBox::Show("Connected to Blood Bank Admin Dashboard!", "Success",
-				MessageBoxButtons::OK, MessageBoxIcon::Information);
-		}
-		else {
-			lblStatus->ForeColor = System::Drawing::Color::Red;
-			lblStatus->Text = "Invalid Username or Password.";
-		}
-	}
-
-	private: System::Void btnSignup_Click(System::Object^ sender, System::EventArgs^ e) {
-		MessageBox::Show("Sign Up functionality will be implemented soon.", "Information",
-			MessageBoxButtons::OK, MessageBoxIcon::Information);
+		// --- LOGIC TO CENTER THE PANEL ---
+	private: System::Void CenterPanel() {
+		int x = (this->ClientSize.Width - loginPanel->Width) / 2;
+		int y = (this->ClientSize.Height - loginPanel->Height) / 2;
+		loginPanel->Location = System::Drawing::Point(x, y);
 	}
 
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-		// Code to run when the form first loads
+		CenterPanel();
+	}
+
+	private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e) {
+		CenterPanel();
+	}
+
+	private: System::Void btnLogin_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (txtUsername->Text == "admin" && txtPassword->Text == "admin123") {
+			MessageBox::Show("Success!", "Login", MessageBoxButtons::OK, MessageBoxIcon::Information);
+		}
+		else {
+			lblStatus->ForeColor = System::Drawing::Color::OrangeRed;
+			lblStatus->Text = "Invalid Credentials!";
+		}
 	}
 	};
 }
