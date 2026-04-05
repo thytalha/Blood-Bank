fdd6b3727945051ca509a513c523f8f1f788250d
thytalha <mtpasha007@gmail.com>
2026-04-06 02:45:17 +0500

    code updated: Added simple login form

--- MyForm.cpp
+++ MyForm.cpp
@@ -7,9 +7,6 @@ using namespace System::Windows::Forms;
 int main(array<String^>^ args) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
-
-    // Ensure "BloodBank" matches your Project Name 
-    // and "MyForm" matches your Form Name
     BloodBank::MyForm form;
     Application::Run(% form);
     return 0;
--- MyForm.h
+++ MyForm.h
@@ -1,6 +1,7 @@
 #pragma once
 
-namespace BloodBank {
+namespace BloodBank
+{
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -43,21 +44,21 @@ namespace BloodBank {
 		void InitializeComponent(void)
 		{
 			this->loginPanel = (gcnew System::Windows::Forms::Panel());
-			this->lblTitle = (gcnew System::Windows::Forms::Label());
-			this->lblUsername = (gcnew System::Windows::Forms::Label());
-			this->txtUsername = (gcnew System::Windows::Forms::TextBox());
-			this->lblPassword = (gcnew System::Windows::Forms::Label());
-			this->txtPassword = (gcnew System::Windows::Forms::TextBox());
-			this->btnLogin = (gcnew System::Windows::Forms::Button());
-			this->btnSignup = (gcnew System::Windows::Forms::Button());
 			this->lblStatus = (gcnew System::Windows::Forms::Label());
+			this->btnSignup = (gcnew System::Windows::Forms::Button());
+			this->btnLogin = (gcnew System::Windows::Forms::Button());
+			this->txtPassword = (gcnew System::Windows::Forms::TextBox());
+			this->lblPassword = (gcnew System::Windows::Forms::Label());
+			this->txtUsername = (gcnew System::Windows::Forms::TextBox());
+			this->lblUsername = (gcnew System::Windows::Forms::Label());
+			this->lblTitle = (gcnew System::Windows::Forms::Label());
 			this->loginPanel->SuspendLayout();
 			this->SuspendLayout();
-
 			// 
 			// loginPanel
 			// 
-			this->loginPanel->BackColor = System::Drawing::Color::FromArgb(15, 30, 70);
+			this->loginPanel->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(15)), static_cast<System::Int32>(static_cast<System::Byte>(30)),
+				static_cast<System::Int32>(static_cast<System::Byte>(70)));
 			this->loginPanel->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->loginPanel->Controls->Add(this->lblStatus);
 			this->loginPanel->Controls->Add(this->btnSignup);
@@ -72,38 +73,46 @@ namespace BloodBank {
 			this->loginPanel->Size = System::Drawing::Size(600, 500);
 			this->loginPanel->TabIndex = 0;
 			// 
-			// lblTitle
+			// lblStatus
 			// 
-			this->lblTitle->AutoSize = true;
-			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 28, System::Drawing::FontStyle::Bold));
-			this->lblTitle->ForeColor = System::Drawing::Color::White;
-			this->lblTitle->Location = System::Drawing::Point(100, 40);
-			this->lblTitle->Name = L"lblTitle";
-			this->lblTitle->Size = System::Drawing::Size(400, 60);
-			this->lblTitle->Text = L"Admin Portal";
+			this->lblStatus->AutoSize = true;
+			this->lblStatus->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
+			this->lblStatus->Location = System::Drawing::Point(80, 440);
+			this->lblStatus->Name = L"lblStatus";
+			this->lblStatus->Size = System::Drawing::Size(0, 23);
+			this->lblStatus->TabIndex = 0;
 			// 
-			// lblUsername
+			// btnSignup
 			// 
-			this->lblUsername->AutoSize = true;
-			this->lblUsername->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
-			this->lblUsername->ForeColor = System::Drawing::Color::LightGray;
-			this->lblUsername->Location = System::Drawing::Point(80, 150);
-			this->lblUsername->Text = L"Username";
+			this->btnSignup->BackColor = System::Drawing::Color::Transparent;
+			this->btnSignup->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btnSignup->FlatAppearance->BorderColor = System::Drawing::Color::White;
+			this->btnSignup->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnSignup->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14));
+			this->btnSignup->ForeColor = System::Drawing::Color::White;
+			this->btnSignup->Location = System::Drawing::Point(310, 370);
+			this->btnSignup->Name = L"btnSignup";
+			this->btnSignup->Size = System::Drawing::Size(210, 50);
+			this->btnSignup->TabIndex = 1;
+			this->btnSignup->Text = L"SIGN UP";
+			this->btnSignup->UseVisualStyleBackColor = false;
+			this->btnSignup->Click += gcnew System::EventHandler(this, &MyForm::btnSignup_Click);
 			// 
-			// txtUsername
+			// btnLogin
 			// 
-			this->txtUsername->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
-			this->txtUsername->Location = System::Drawing::Point(80, 185);
-			this->txtUsername->Name = L"txtUsername";
-			this->txtUsername->Size = System::Drawing::Size(440, 39);
-			// 
-			// lblPassword
-			// 
-			this->lblPassword->AutoSize = true;
-			this->lblPassword->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
-			this->lblPassword->ForeColor = System::Drawing::Color::LightGray;
-			this->lblPassword->Location = System::Drawing::Point(80, 250);
-			this->lblPassword->Text = L"Password";
+			this->btnLogin->BackColor = System::Drawing::Color::Crimson;
+			this->btnLogin->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btnLogin->FlatAppearance->BorderSize = 0;
+			this->btnLogin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnLogin->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14));
+			this->btnLogin->ForeColor = System::Drawing::Color::White;
+			this->btnLogin->Location = System::Drawing::Point(80, 370);
+			this->btnLogin->Name = L"btnLogin";
+			this->btnLogin->Size = System::Drawing::Size(210, 50);
+			this->btnLogin->TabIndex = 2;
+			this->btnLogin->Text = L"LOGIN";
+			this->btnLogin->UseVisualStyleBackColor = false;
+			this->btnLogin->Click += gcnew System::EventHandler(this, &MyForm::btnLogin_Click);
 			// 
 			// txtPassword
 			// 
@@ -112,50 +121,55 @@ namespace BloodBank {
 			this->txtPassword->Name = L"txtPassword";
 			this->txtPassword->PasswordChar = '*';
 			this->txtPassword->Size = System::Drawing::Size(440, 39);
+			this->txtPassword->TabIndex = 3;
 			// 
-			// btnLogin
+			// lblPassword
 			// 
-			this->btnLogin->BackColor = System::Drawing::Color::Crimson;
-			this->btnLogin->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btnLogin->FlatAppearance->BorderSize = 0;
-			this->btnLogin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->btnLogin->Font = (gcnew System::Drawing::Font(L"Segoe UI Bold", 14));
-			this->btnLogin->ForeColor = System::Drawing::Color::White;
-			this->btnLogin->Location = System::Drawing::Point(80, 370);
-			this->btnLogin->Name = L"btnLogin";
-			this->btnLogin->Size = System::Drawing::Size(210, 50);
-			this->btnLogin->Text = L"LOGIN";
-			this->btnLogin->UseVisualStyleBackColor = false;
-			this->btnLogin->Click += gcnew System::EventHandler(this, &MyForm::btnLogin_Click);
+			this->lblPassword->AutoSize = true;
+			this->lblPassword->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
+			this->lblPassword->ForeColor = System::Drawing::Color::LightGray;
+			this->lblPassword->Location = System::Drawing::Point(80, 250);
+			this->lblPassword->Name = L"lblPassword";
+			this->lblPassword->Size = System::Drawing::Size(111, 32);
+			this->lblPassword->TabIndex = 4;
+			this->lblPassword->Text = L"Password";
 			// 
-			// btnSignup
+			// txtUsername
 			// 
-			this->btnSignup->BackColor = System::Drawing::Color::Transparent;
-			this->btnSignup->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btnSignup->FlatAppearance->BorderColor = System::Drawing::Color::White;
-			this->btnSignup->FlatAppearance->BorderSize = 1;
-			this->btnSignup->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->btnSignup->Font = (gcnew System::Drawing::Font(L"Segoe UI Bold", 14));
-			this->btnSignup->ForeColor = System::Drawing::Color::White;
-			this->btnSignup->Location = System::Drawing::Point(310, 370);
-			this->btnSignup->Name = L"btnSignup";
-			this->btnSignup->Size = System::Drawing::Size(210, 50);
-			this->btnSignup->Text = L"SIGN UP";
-			this->btnSignup->UseVisualStyleBackColor = false;
-			this->btnSignup->Click += gcnew System::EventHandler(this, &MyForm::btnSignup_Click);
+			this->txtUsername->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
+			this->txtUsername->Location = System::Drawing::Point(80, 185);
+			this->txtUsername->Name = L"txtUsername";
+			this->txtUsername->Size = System::Drawing::Size(440, 39);
+			this->txtUsername->TabIndex = 5;
 			// 
-			// lblStatus
+			// lblUsername
 			// 
-			this->lblStatus->AutoSize = true;
-			this->lblStatus->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
-			this->lblStatus->Location = System::Drawing::Point(80, 440);
-			this->lblStatus->Name = L"lblStatus";
+			this->lblUsername->AutoSize = true;
+			this->lblUsername->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
+			this->lblUsername->ForeColor = System::Drawing::Color::LightGray;
+			this->lblUsername->Location = System::Drawing::Point(80, 150);
+			this->lblUsername->Name = L"lblUsername";
+			this->lblUsername->Size = System::Drawing::Size(121, 32);
+			this->lblUsername->TabIndex = 6;
+			this->lblUsername->Text = L"Username";
+			// 
+			// lblTitle
+			// 
+			this->lblTitle->AutoSize = true;
+			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 28, System::Drawing::FontStyle::Bold));
+			this->lblTitle->ForeColor = System::Drawing::Color::White;
+			this->lblTitle->Location = System::Drawing::Point(100, 40);
+			this->lblTitle->Name = L"lblTitle";
+			this->lblTitle->Size = System::Drawing::Size(306, 62);
+			this->lblTitle->TabIndex = 7;
+			this->lblTitle->Text = L"Admin Portal";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::FromArgb(5, 15, 35);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(5)), static_cast<System::Int32>(static_cast<System::Byte>(15)),
+				static_cast<System::Int32>(static_cast<System::Byte>(35)));
 			this->ClientSize = System::Drawing::Size(1200, 800);
 			this->Controls->Add(this->loginPanel);
 			this->Name = L"MyForm";
@@ -166,36 +180,44 @@ namespace BloodBank {
 			this->loginPanel->ResumeLayout(false);
 			this->loginPanel->PerformLayout();
 			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 
-	private: System::Void CenterPanel() {
+	private: System::Void CenterPanel()
+	{
 		int x = (this->ClientSize.Width - loginPanel->Width) / 2;
 		int y = (this->ClientSize.Height - loginPanel->Height) / 2;
 		loginPanel->Location = System::Drawing::Point(x, y);
 	}
 
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) 
+	{
 		CenterPanel();
 	}
 
-	private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e) 
+	{
 		CenterPanel();
 	}
 
-	private: System::Void btnLogin_Click(System::Object^ sender, System::EventArgs^ e) {
-		if (txtUsername->Text == "admin" && txtPassword->Text == "admin123") {
+	private: System::Void btnLogin_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		if (txtUsername->Text == "admin" && txtPassword->Text == "admin123")
+		{
 			MessageBox::Show("Admin Verified. Loading Database...", "Access Granted",
 				MessageBoxButtons::OK, MessageBoxIcon::Information);
 		}
-		else {
+		else
+		{
 			lblStatus->ForeColor = System::Drawing::Color::OrangeRed;
 			lblStatus->Text = "Error: Invalid admin credentials.";
 		}
 	}
 
 		   // THE UPDATED SIGN UP MESSAGE:
-	private: System::Void btnSignup_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void btnSignup_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
 		MessageBox::Show("The Sign Up form is not ready yet. It will be ready soon!", "System Update",
 			MessageBoxButtons::OK, MessageBoxIcon::Information);
 	}
