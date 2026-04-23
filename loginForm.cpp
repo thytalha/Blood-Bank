57d7eae48b049cccd59f3408d8c907deb26c8fcc
thytalha <mtpasha007@gmail.com>
2026-04-23 13:31:25 +0500

    Renamed myForm to loginForm

--- loginForm.cpp
+++ loginForm.cpp
@@ -1,4 +1,4 @@
-#include "MyForm.h"
+#include "loginForm.h"
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
@@ -6,8 +6,8 @@ int main(array<String^>^ args)
 {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
-    BloodBank::MyForm form;
+    BloodBank::loginForm form;
     Application::Run(% form);
     return 0;
 }
-    //code updated by Maryam 
+    //code updated by Talha
